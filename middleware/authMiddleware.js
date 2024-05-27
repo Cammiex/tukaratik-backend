bf7d4649c4e83ed5902d188e3c94b885d945003f
Siti Rahmah <strahmasti@gmail.com>
2024-05-27 23:13:33 +0800

    fix: auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,13 @@
-const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
 
 module.exports = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+    const userId = req.headers['user-id'];
 
-    if (!token) return res.sendStatus(401);
+    if (!userId) return res.sendStatus(401);
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-        if (err || !decodedToken.userId) return res.sendStatus(403); // Menangani kasus token tidak valid
-        req.user = decodedToken.userId;
+    User.findById(userId, (err, results) => {
+        if (err || results.length === 0) return res.sendStatus(403);
+        req.userId = userId;
         next();
     });
 };
