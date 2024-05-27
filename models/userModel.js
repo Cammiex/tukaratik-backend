51f500cad8dcd06f171aa46b74c96512e1bd0b84
Siti Rahmah <strahmasti@gmail.com>
2024-05-27 23:11:38 +0800

    fix: user model

--- models/userModel.js
+++ models/userModel.js
@@ -5,9 +5,21 @@ const User = {
         const query = 'INSERT INTO Users (username, password_hash, email) VALUES (?, ?, ?)';
         db.query(query, [username, passwordHash, email], callback);
     },
+    findByUsername: (username, callback) => {
+        const query = 'SELECT * FROM Users WHERE username = ?';
+        db.query(query, [username], callback);
+    },
     findByEmail: (email, callback) => {
         const query = 'SELECT * FROM Users WHERE email = ?';
         db.query(query, [email], callback);
+    },
+    findById: (userId, callback) => {
+        const query = 'SELECT * FROM Users WHERE user_id = ?';
+        db.query(query, [userId], callback);
+    },
+    updatePoints: (userId, points, callback) => {
+        const query = 'UPDATE Users SET total_points = total_points + ? WHERE user_id = ?';
+        db.query(query, [points, userId], callback);
     }
 };
 
