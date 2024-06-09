d0fdc6eb8894e7ac549a1182799cbd2fc370376e
Siti Rahmah <strahmasti@gmail.com>
2024-06-09 19:48:42 +0800

    add multer

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const userRoutes = require('./routes/userRoutes');
 const wasteRoutes = require('./routes/wasteRoutes');
 const productRoutes = require('./routes/productRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-// const transactionRoutes = require('./routes/transactionRoutes');
-const profileRoutes = require('./routes/profileRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use((req, res, next) => {
     console.log(`${req.method} request for '${req.url}'`);
@@ -23,9 +22,6 @@ app.use((req, res, next) => {
 app.use('/users', userRoutes);
 app.use('/waste', wasteRoutes);
 app.use('/products', productRoutes);
-app.use('/cart', cartRoutes);
-// app.use('/transactions', transactionRoutes);
-app.use('/', profileRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
