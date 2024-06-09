e6769e2d0f1c15881a6904db3ed88024dcc9af6a
Siti Rahmah <strahmasti@gmail.com>
2024-06-09 19:49:46 +0800

    fix db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
+
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to MySQL database');
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.stack);
+        return;
+    }
+    console.log('Connected to database.');
 });
 
-module.exports = connection;
+module.exports = db;
