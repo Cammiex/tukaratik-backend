db46bdffe600569204ca85bded916a6d0b0566a8
Siti Rahmah <strahmasti@gmail.com>
2024-06-11 14:47:27 +0800

    fix: typo user

--- models/userModel.js
+++ models/userModel.js
@@ -2,35 +2,35 @@ const db = require('../config/db');
 
 const User = {
     create: (username, passwordHash, email, callback) => {
-        const query = 'INSERT INTO Users (username, password_hash, email) VALUES (?, ?, ?)';
+        const query = 'INSERT INTO users (username, password_hash, email) VALUES (?, ?, ?)';
         db.query(query, [username, passwordHash, email], callback);
     },
     findByUsername: (username, callback) => {
-        const query = 'SELECT * FROM Users WHERE username = ?';
+        const query = 'SELECT * FROM users WHERE username = ?';
         db.query(query, [username], callback);
     },
     findByEmail: (email, callback) => {
-        const query = 'SELECT * FROM Users WHERE email = ?';
+        const query = 'SELECT * FROM users WHERE email = ?';
         db.query(query, [email], callback);
     },
     findById: (userId, callback) => {
-        const query = 'SELECT * FROM Users WHERE user_id = ?';
+        const query = 'SELECT * FROM users WHERE user_id = ?';
         db.query(query, [userId], callback);
     },
     delete: (userId, callback) => {
-        const query = 'DELETE FROM Users WHERE user_id = ?';
+        const query = 'DELETE FROM users WHERE user_id = ?';
         db.query(query, [userId], callback);
     },
     getAll: (callback) => {
-        const query = 'SELECT * FROM Users';
+        const query = 'SELECT * FROM users';
         db.query(query, callback);
     },
     getTotalPoints: (userId, callback) => {
-        const query = 'SELECT total_points FROM Users WHERE user_id = ?';
+        const query = 'SELECT total_points FROM users WHERE user_id = ?';
         db.query(query, [userId], callback);
     },
     updatePoints: (userId, points, callback) => {
-        const query = 'UPDATE Users SET total_points = total_points + ? WHERE user_id = ?';
+        const query = 'UPDATE users SET total_points = total_points + ? WHERE user_id = ?';
         db.query(query, [points, userId], callback);
     }
 };
