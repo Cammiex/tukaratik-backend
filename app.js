01feb3bac4d2b9095e1f4f3bab7f97fb680d93e3
Siti Rahmah <strahmasti@gmail.com>
2024-06-11 09:45:09 +0800

    chore(app): update app.js configuration

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const moment = require('moment');
 
 const userRoutes = require('./routes/userRoutes');
 const wasteRoutes = require('./routes/wasteRoutes');
 const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +25,8 @@ app.use((req, res, next) => {
 app.use('/users', userRoutes);
 app.use('/waste', wasteRoutes);
 app.use('/products', productRoutes);
+app.use('/cart', cartRoutes);
+app.use('/orders', orderRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
